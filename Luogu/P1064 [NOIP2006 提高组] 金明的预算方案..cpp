87d72002abf25b5b0ce7fce68743d9c245b212c0
Zlin <2768228421@qq.com>
2024-06-25 00:23:48 +0800

    P1064

--- Luogu/P1064 [NOIP2006 提高组] 金明的预算方案..cpp
+++ Luogu/P1064 [NOIP2006 提高组] 金明的预算方案..cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
+int n, m, ans = 0;
+int l, r;
 int dp[40010]{}, v[65]{}, p[65]{}, q[65]{};
 vector<vector<int>> qq(65);
 
@@ -15,21 +16,30 @@ int main()
     {
         cin >> v[i] >> p[i] >> q[i];
         if (q[i])
-            qq[i].push_back(i);
+            qq[q[i]].push_back(i);
     }
     for (int i = 1; i <= m; i++)
     {
         if (!q[i])
         {
-            if (qq[i].size() == 0)
-            {
-            }
+            l = 0, r = 0;
             if (qq[i].size() == 1)
-            {
-            }
+                l = qq[i][0];
             if (qq[i].size() == 2)
+                l = qq[i][0], r = qq[i][1];
+            for (int j = n; j >= v[i]; j--)
             {
+                dp[j] = max(dp[j], dp[j - v[i]] + v[i] * p[i]);
+                if (l && j >= v[l] + v[i])
+                    dp[j] = max(dp[j], dp[j - v[i] - v[l]] + v[i] * p[i] + v[l] * p[l]);
+                if (r && j >= v[r] + v[i])
+                    dp[j] = max(dp[j], dp[j - v[i] - v[r]] + v[i] * p[i] + v[r] * p[r]);
+                if (l && r && j >= v[i] + v[l] + v[r])
+                    dp[j] = max(dp[j], dp[j - v[i] - v[l] - v[r]] + v[i] * p[i] + v[l] * p[l] + v[r] * p[r]);
             }
         }
     }
+    for (int i = 1; i <= n; i++)
+        ans = max(ans, dp[i]);
+    cout << ans;
 }
