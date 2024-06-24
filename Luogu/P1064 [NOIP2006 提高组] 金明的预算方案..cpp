c84b396245c487b9d6da599a110f3ac0c0c10b9d
Zlin <2768228421@qq.com>
2024-06-24 23:53:09 +0800

    dp

--- Luogu/P1064 [NOIP2006 提高组] 金明的预算方案..cpp
+++ Luogu/P1064 [NOIP2006 提高组] 金明的预算方案..cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
+int dp[40010]{}, v[65]{}, p[65]{}, q[65]{};
+vector<vector<int>> qq(65);
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++)
+    {
+        cin >> v[i] >> p[i] >> q[i];
+        if (q[i])
+            qq[i].push_back(i);
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        if (!q[i])
+        {
+            if (qq[i].size() == 0)
+            {
+            }
+            if (qq[i].size() == 1)
+            {
+            }
+            if (qq[i].size() == 2)
+            {
+            }
+        }
+    }
 }
