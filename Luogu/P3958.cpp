cf54d6647e096e931a9525049454a0fe9a52f617
Zlin <2768228421@qq.com>
2024-07-02 09:58:28 +0800

    7.1

--- Luogu/P3958.cpp
+++ Luogu/P3958.cpp
@@ -5,9 +5,31 @@ using ll = long long;
 using vi = vector<int>;
 using vll = vector<long long>;
 
+int n, h, r;
+
+struct ss
+{
+    double x, y, z;
+} T[1010]{};
+
+double cal(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    return sqrt(pow(xa - xb, 2) + pow(ya - yb, 2) + pow(za - zb, 2));
+}
+
+void solve()
+{
+    cin >> n >> h >> r;
+    
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    int TTT;
+    cin >> TTT;
+    while (TTT--)
+        solve();
 }
