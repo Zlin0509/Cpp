bbfc6e53f050b5c1706ae8d197d2fc554e0d6bec
Zlin <2768228421@qq.com>
2024-07-03 19:56:33 +0800

    7.3

---
+++ Luogu/P5012.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+int n, t, a, b, x, y;
+int A[100010]{};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> t;
+    for (int i = 1; i <= n; i++)
+        cin >> A[i];
+
+    int l, r;
+    while (t--)
+    {
+        cin>>a>>b>>x>>y;
+        
+    }
+}
