101dcd4eeadd185ea202595d597210bf52646d73
Zlin <2768228421@qq.com>
2024-07-07 09:16:39 +0800

    7.6

--- Luogu/P1966.cpp
+++ Luogu/P1966.cpp
@@ -6,10 +6,28 @@ using vi = vector<int>;
 using vll = vector<ll>;
 
 const ll Mod = 1e8 - 3;
+int n;
+int b[200010]{};
+struct ss
+{
+    int a, id;
+} a[200010]{};
+vector<queue<int>> p(200010);
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> b[i];
+        p[b[i]].push(i);
+    }
+    sort(b[1], b[1 + n]);
 }
