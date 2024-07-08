550061d61d22b5e8655c700c560a25fede9aef7c
Zlin <2768228421@qq.com>
2024-07-08 09:04:14 +0800

    7.7

--- Gaming/E.cpp
+++ Gaming/E.cpp
@@ -1,31 +1,16 @@
 #include "bits/stdc++.h"
 using namespace std;
-
-using ll = long long;
-using vi = vector<int>;
-using vll = vector<long long>;
-
 int n, z;
 int a[200010]{};
-
 bool check(int k)
 {
     for (int i = 1; i <= k; i++)
-    {
         if (a[n - k + i] - a[i] < z)
-        {
             return true;
-        }
-    }
     return false;
 }
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
     cin >> n >> z;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
@@ -34,10 +19,7 @@ int main()
     while (l < r)
     {
         mid = l + r + 1 >> 1;
-        if (check(mid))
-            r = mid - 1;
-        else
-            l = mid;
+        check(mid) ? r = mid - 1 : l = mid;
     }
     cout << l;
 }
