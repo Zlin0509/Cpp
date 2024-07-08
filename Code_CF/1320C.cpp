26ed97e19cfab90781248b1915f7e4b3bff326e5
Zlin <2768228421@qq.com>
2024-07-08 11:18:47 +0800

    7.8-lunch

--- Code_CF/1320C.cpp
+++ Code_CF/1320C.cpp
@@ -7,8 +7,8 @@ using vll = vector<long long>;
 using vi = vector<int>;
 const int N = 2e5 + 10;
 const ll INF = 1e12;
-int n, m, p;
-ll ans = -INF;
+int n, m, p, l, r, mid;
+ll ans;
 struct ss1
 {
     int val, cost;
@@ -56,7 +56,7 @@ inline void read()
     sort(b1 + 1, b1 + 1 + m, cmp2);
     sort(c + 1, c + 1 + p, cmp3);
     // 手动离散化，结构体不会，，，，
-    int n1 = 1, m1 = 1;
+    int n1 = 0, m1 = 0;
     for (int i = 1; i <= n; i++)
         if (a1[i].val != a[n1].val)
             a[++n1] = a1[i];
@@ -71,8 +71,8 @@ inline void build(int l, int r, int i)
     T[i].l = l, T[i].r = r;
     if (l == r)
     {
-        T[i].val = -a[l].val;
-        T[i].Max = -a[l].val;
+        T[i].val = -a[l].cost;
+        T[i].Max = -a[l].cost;
         return;
     }
     int mid = l + r >> 1;
@@ -87,8 +87,11 @@ inline void build(int l, int r, int i)
 inline void push_down(int i)
 {
     T[i].Max += T[i].tag;
-    T[i << 1].tag = T[i].tag;
-    T[i << 1 | 1].tag = T[i].tag;
+    if (T[i].l != T[i].r)
+    {
+        T[i << 1].tag += T[i].tag;
+        T[i << 1 | 1].tag += T[i].tag;
+    }
     T[i].tag = 0;
 }
 
@@ -112,14 +115,20 @@ inline void change(int l, int r, int i, int k)
 inline void Main()
 {
     int it1 = 1, it2 = 1;
+    ans = -a[1].cost - b[1].cost;
     while (it1 <= m)
     {
         while (it2 <= p && c[it2].val2 < b[it1].val)
         {
-            auto l = upper_bound(a, a + n, c[it2].val1) - a;
-            if (l == n + 1)
+            l = 1, r = n;
+            while (l < r)
+            {
+                mid = l + r >> 1;
+                a[mid].val <= c[it2].val1 ? l = mid + 1 : r = mid;
+            }
+            if (a[l].val > c[it2].val1)
                 change(l, n, 1, c[it2].cost);
-            ans = max(ans, T[1].Max - b[it1].cost);
+            ans = max(ans, T[1].Max + T[1].tag - b[it1].cost);
             ++it2;
         }
         ++it1;
