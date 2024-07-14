33f2ce524e762a70c3b07703dfbf934b7c56659b
Zlin <2768228421@qq.com>
2024-07-14 16:22:17 +0800

    7.14

--- Luogu/P3960.cpp
+++ Luogu/P3960.cpp
@@ -6,23 +6,19 @@ using namespace std;
 #define vll vector<ll>
 const int N = 3e5 + 10;
 
-int n, m, q;
-struct ss
-{
-    int x, y, id;
-    friend bool operator<(const ss &a, const ss &b) { return a.x == b.x ? a.id < b.id : a.x < b.x; }
-} c[N];
-int t[N], change[N], ans[N], len[N], len2[N], h[N], arr[N * 3];
-bool cmp(const ss &a, const ss &b) { return a.id < b.id; }
-void add(int *tree, int size, int i, int k)
+ll n, m, q;
+ll x[N], y[N], I[N];
+ll t[N * 3], change[N], ans[N], len[N], len2[N], h[N], arr[N * 3];
+bool cmp(ll p1, ll p2) { return x[p1] == x[p2] ? p1 < p2 : x[p1] < x[p2]; }
+void add(ll *tree, ll size, ll i, ll k)
 {
     for (; i <= size; i += lowbit(i))
         tree[i] += k;
 }
 
-int query(int *tree, int l, int r)
+ll query(ll *tree, ll l, ll r)
 {
-    int sum = 0;
+    ll sum = 0;
     for (; r; r -= lowbit(r))
         sum += tree[r];
     --l;
@@ -31,7 +27,7 @@ int query(int *tree, int l, int r)
     return sum;
 }
 
-int search(int *arr, int size, int num)
+ll search(ll *arr, ll size, ll num)
 {
     int l = 1, r = size, mid;
     while (l < r)
@@ -49,36 +45,60 @@ void Zlin()
 {
     cin >> n >> m >> q;
     for (int i = 1; i <= q; i++)
-        cin >> c[i].x >> c[i].y, c[i].id = i;
-    sort(c + 1, c + 1 + q);
+        cin >> x[i] >> y[i], I[i] = i;
+    sort(I + 1, I + 1 + q, cmp);
+    for (int i = 1; i < m; i++)
+        add(t, m - 1, i, 1);
     for (int i = 1; i <= n; i++)
         len[i] = m - 1;
     for (int i = 1, top = 0, pos; i <= q; i++)
     {
-        if (c[i - 1].x != c[i].x)
+        if (x[I[i - 1]] != x[I[i]])
             while (top)
                 add(t, m - 1, change[top--], 1);
-        if (c[i].y > len[c[i].x])
+        if (y[I[i]] > len[x[I[i]]])
             continue;
-        pos = search(t, m - 1, c[i].y);
+        pos = search(t, m - 1, y[I[i]]);
         add(t, m - 1, pos, -1);
         change[++top] = pos;
-        --len[c[i].x];
-        ans[i] = (c[i].x - 1) * m + pos;
+        --len[x[I[i]]];
+        ans[I[i]] = (x[I[i]] - 1) * m + pos;
     }
-    sort(c + 1, c + 1 + 1, cmp);
     memset(t, 0, sizeof(t));
-    for (int i = 1; i <= n; i++)
-        arr[q + i] = i * m, len2[i] = m - 1, len[i] = 0;
     len[n + 1] = n, h[n + 1] = q;
+    for (int i = 1, iter = 0; i <= n; i++)
+    {
+        while (iter <= q && x[I[iter]] < i)
+            ++iter;
+        h[i] = iter - 1;
+    }
+    for (int i = 1; i <= n; i++)
+        arr[q + i] = i * m, len2[i] = m - 1, len[i] = 0, add(t + h[n + 1], n + q, i, 1);
     for (int i = 1, pos; i <= q; i++)
     {
-
+        pos = search(t + h[n + 1], n + q, x[i]);
+        add(t + h[n + 1], n + q, pos, -1);
+        add(t + h[n + 1], n + q, ++len[n + 1], 1);
         if (ans[i])
         {
+            arr[len[n + 1] + h[n + 1]] = ans[i];
+            add(t + h[x[i]], h[x[i] + 1] - h[x[i]], ++len[x[i]], 1);
+            arr[len[x[i]] + h[x[i]]] = arr[h[n + 1] + pos];
+            --len2[x[i]];
         }
         else
         {
+            if (y[i] != m)
+            {
+                int pos2 = search(t + h[x[i]], h[x[i] + 1] - h[x[i]], y[i] - len2[x[i]]);
+                add(t + h[x[i]], h[x[i] + 1] - h[x[i]], pos2, -1);
+                ans[i] = arr[h[x[i]] + pos2];
+                add(t + h[x[i]], h[x[i] + 1] - h[x[i]], ++len[x[i]], 1);
+                arr[h[x[i]] + len[x[i]]] = arr[h[n + 1] + pos];
+            }
+            else
+                ans[i] = arr[h[n + 1] + pos];
+            arr[h[n + 1] + len[n + 1]] = ans[i];
         }
     }
 
