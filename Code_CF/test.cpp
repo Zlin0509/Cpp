c692c747760ad989b4793fda32158ef02b2441ce
zlin <2768228421@qq.com>
2024-08-13 11:59:06 +0800

    321

--- Code_CF/test.cpp
+++ Code_CF/test.cpp
@@ -1,8 +1,15 @@
 #include "bits/stdc++.h"
 using namespace std;
+vector<int> a;
 int main()
 {
     int ttt;
     cin >> ttt;
-    cout << ttt;
+    for (int i = 1, x; i <= ttt; i++)
+    {
+        cin >> x;
+        a.push_back(x);
+    }
+    for (auto it : a)
+        cout << it << ' ';
 }
